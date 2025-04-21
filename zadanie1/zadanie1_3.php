9c8e717d742b5f5ee03177e6c4260bbf60650520
Andrey Kharchenko <andrey.kharchenko22@gmail.com>
2025-04-21 20:23:24 +0300

    zadanie1

---
+++ zadanie1/zadanie1_3.php
@@ -0,0 +1,44 @@
+php<?php
+$a = readline(prompt: "Введите месяц(1-12): ");
+switch ($a) {
+    case 1:
+        echo "Январь 31 день";
+        break;
+    case 2:
+        echo "Февраль 28 дней (високосный год 29 дней)";
+        break;
+    case 3:
+        echo "Март 31 день";
+        break;
+    case 4:
+        echo "Апрель 30 дней";
+        break;
+    case 5:
+        echo "Май 31 день";
+        break;
+    case 6:
+        echo "Июнь 30 дней";
+        break;
+    case 7:
+        echo "Июль 31 день";
+        break;
+    case 8:
+        echo "Август 31 день";
+        break;
+    case 9:
+        echo "Сентябрь 30 дней";
+        break;
+    case 10:
+        echo "Октябрь 31 день";
+        break;
+    case 11:
+        echo "Ноябрь 30 дней";
+        break;
+    case 12:
+        echo "Декабрь 31 день";
+        break;
+    default:
+        echo "Такого месяца не существует! Пожалуйста, введите число от 1 до 12.";
+        break;
+}
+?>
